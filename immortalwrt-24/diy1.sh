13d129ba2cdfcd5a302e969760dd88855577c503
aoxiyu <127184306+aoxiyu@users.noreply.github.com>
2025-06-04 01:35:05 +0800

    Update diy1.sh

--- immortalwrt-24/diy1.sh
+++ immortalwrt-24/diy1.sh
@@ -10,9 +10,24 @@
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
 
-# 自定义软件源
+# Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-sed -i '$a src-git small8 https://github.com/kenzok8/small-package' feeds.conf.default
-
+sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
 # Easytier
 git clone --depth 1 -b main https://github.com/EasyTier/luci-app-easytier.git package/package-easytier
+# 更新所有 feeds
+./scripts/feeds update -a
+# 删除可能冲突或重复的包
+rm -rf feeds/luci/applications/luci-app-mosdns
+rm -rf feeds/packages/net/{alist,adguardhome,mosdns,xray*,v2ray*,sing*,smartdns}
+rm -rf feeds/packages/utils/v2dat
+rm -rf feeds/packages/lang/golang
+# 替换 golang 包为 kenzok8 的版本（支持 golang 1.22+）
+git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
+
+# 安装所有 feeds
+./scripts/feeds install -a
+
+# 去掉 gn 包的 -Werror，避免编译因警告失败
+find feeds/packages/devel/gn -type f -exec sed -i 's/-Werror//g' {} +
