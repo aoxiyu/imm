d2660da3496411b888d00d7ed6786b84cf37f115
aoxiyu <127184306+aoxiyu@users.noreply.github.com>
2025-06-04 01:56:07 +0800

    Update diy1.sh

--- immortalwrt-24/diy1.sh
+++ immortalwrt-24/diy1.sh
@@ -12,8 +12,9 @@
 
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+# 添加第三方软件源，防止依赖缺失
 sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
+sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
 # Easytier
 git clone --depth 1 -b main https://github.com/EasyTier/luci-app-easytier.git package/package-easytier
 # 更新所有 feeds
