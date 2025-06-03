645b8fcdc09ef09dd694d66a6d4ba47e0556d51f
aoxiyu <127184306+aoxiyu@users.noreply.github.com>
2025-06-04 02:35:29 +0800

    Update diy1.sh

--- immortalwrt-24/diy1.sh
+++ immortalwrt-24/diy1.sh
@@ -1,31 +1,23 @@
 #!/bin/bash
-#
-# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
-#
-# https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part1.sh
-# Description: OpenWrt DIY script part 1 (Before Update feeds)
-#
 
-# Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 # 添加第三方软件源，防止依赖缺失
 sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
+
 # Easytier
 git clone --depth 1 -b main https://github.com/EasyTier/luci-app-easytier.git package/package-easytier
+
 # 更新所有 feeds
 ./scripts/feeds update -a
-# 删除可能冲突或重复的包
-rm -rf feeds/luci/applications/luci-app-mosdns
-rm -rf feeds/packages/net/{alist,adguardhome,mosdns,xray*,v2ray*,sing*,smartdns}
-rm -rf feeds/packages/utils/v2dat
-rm -rf feeds/packages/lang/golang
+
+# 不删除 small 依赖的包，避免依赖缺失
+# rm -rf feeds/luci/applications/luci-app-mosdns
+# rm -rf feeds/packages/net/{alist,adguardhome,mosdns,xray*,v2ray*,sing*,smartdns}
+# rm -rf feeds/packages/utils/v2dat
+
 # 替换 golang 包为 kenzok8 的版本（支持 golang 1.22+）
-git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
+rm -rf feeds/packages/lang/golang
+git clone --depth 1 https://github.com/kenzok8/golang feeds/packages/lang/golang
 
 # 安装所有 feeds
 ./scripts/feeds install -a
